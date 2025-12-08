1d2b9975ee06a57d986351274813be48ff9668ad
Williamc2b <150145033+Williamc2b@users.noreply.github.com>
2025-12-08 20:31:22 +0000

    Added Assets for Proof of concept prototyping
    Preliminary script added as well

---
+++ Assets/Scripts/LaneControls.cs
@@ -0,0 +1,19 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class LaneControls : MonoBehaviour
+{
+    public InputActionReference joystickAction;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
