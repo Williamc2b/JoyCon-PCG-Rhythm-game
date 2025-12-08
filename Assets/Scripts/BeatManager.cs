ba65a1bd22baab3b8ca1c1f37de5b9b998fb0712
Williamc2b <150145033+Williamc2b@users.noreply.github.com>
2025-12-08 20:45:58 +0000

    Initial script concept

--- Assets/Scripts/BeatManager.cs
+++ Assets/Scripts/BeatManager.cs
@@ -3,15 +3,25 @@ using UnityEngine;
 public class BeatManager : MonoBehaviour
 {
     public GameObject[] Lanes;
+    public GameObject[] notes;
+    public float noteSpawnFrequency;
+    public float tempo;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
-
+    void spawnRandomNote()
+    {
+        int laneIndex = Random.Range(0, Lanes.Length);
+        GameObject lane = Lanes[laneIndex];
+        int noteIndex = Random.Range(0, notes.Length);
+        GameObject note = notes[noteIndex];
+        Instantiate(note, lane.transform.position, Quaternion.identity);
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        spawnRandomNote();
     }
 }
