878dfa24cb8676c51552334f27d7fccaaf9adfce
Williamc2b <150145033+Williamc2b@users.noreply.github.com>
2025-12-09 00:54:02 +0000

    Added note hit and note miss implementation

--- Assets/Scripts/LaneControls.cs
+++ Assets/Scripts/LaneControls.cs
@@ -1,19 +1,48 @@
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-public class LaneControls : MonoBehaviour
+using System.Collections.Generic;
+
+public class Lane : MonoBehaviour
 {
-    public InputActionReference joystickAction;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public InputActionReference stickAction;
+    public Transform hitLine; // The line where notes should be hit
+    public float hitWindow = 0.2f; // Time window for hitting notes
+    private Vector2 lastStickInput;
+    private float flickCooldown = 0.15f;
+    private float lastFlickTime;
+    public bool flickedThisFrame = false;
     void Start()
     {
-        
+        stickAction.action.Enable();
     }
+    
+    private bool flickDetected = false;
 
-    // Update is called once per frame
     void Update()
     {
+        Vector2 currentStick = stickAction.action.ReadValue<Vector2>();
         
+        // Check if we're still in cooldown
+        bool canFlick = Time.time - lastFlickTime > flickCooldown;
+        
+        if (canFlick && lastStickInput.magnitude < 0.3f && currentStick.magnitude > 0.5f)
+        {
+            flickDetected = true;
+            flickedThisFrame = true;
+            lastFlickTime = Time.time;
+            Debug.Log($"Flick detected at {Time.time}");
+        }
+        else
+        {
+            flickedThisFrame = false;
+        }
+        
+        // Reset flickDetected after processing
+        if (flickDetected)
+        {
+            flickDetected = false;
+        }
+        
+        lastStickInput = currentStick;
     }
 }
--- Assets/Scripts/NoteControls.cs
+++ Assets/Scripts/NoteControls.cs
@@ -5,15 +5,35 @@ public class NoteControls : MonoBehaviour
     private float beattempo;
     public GameObject JudgementLine;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Lane lane;
     void Start()
     {
-        
+        lane = JudgementLine.GetComponent<Lane>();
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, JudgementLine.transform.position, beattempo * Time.deltaTime);
+        bool isflicked = lane.flickedThisFrame;
+        if (isflicked)
+        {
+            float distanceToLine = Vector3.Distance(transform.position, JudgementLine.transform.position);
+            if (distanceToLine < 0.6f)
+            {
+                Destroy(gameObject);
+                Debug.Log("Note Hit!");
+            }
+        }
+        else
+        {
+            if (Vector3.Distance(transform.position, JudgementLine.transform.position) < 0.1f)
+            {
+                Destroy(gameObject);
+                Debug.Log("Missed Note!");
+            }
+        }
+
     }
     public void setbeattempo(float tempo)
     {
